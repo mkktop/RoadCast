0b08cafa9400edf36568af888a25f17f1042843a
mkk <2864078813@qq.com>
2026-04-07 21:35:29 +0800

    fix: 添加缺失的clickable import

--- app/src/main/java/com/roadcast/app/ui/screens/ConfigScreen.kt
+++ app/src/main/java/com/roadcast/app/ui/screens/ConfigScreen.kt
@@ -1,5 +1,6 @@
 package com.roadcast.app.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
